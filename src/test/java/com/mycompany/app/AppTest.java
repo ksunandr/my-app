d3eb7953d122e56fd6da7aebf402f8d2d3a03529
ksan <kseniia.andreeva@diffblue.com>
2024-01-08 21:08:17 +0000

    211

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -14,5 +14,68 @@ class AppTest {
         assertFalse((new App()).ifYes("foo"));
         assertTrue((new App()).ifYes("Yes"));
     }
+
+    /**
+     * Method under test: {@link App#ifYes1(String)}
+     */
+    @Test
+    void testIfYes1() {
+        assertFalse((new App()).ifYes1("foo"));
+        assertTrue((new App()).ifYes1("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes2(String)}
+     */
+    @Test
+    void testIfYes2() {
+        assertFalse((new App()).ifYes2("foo"));
+        assertTrue((new App()).ifYes2("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes3(String)}
+     */
+    @Test
+    void testIfYes3() {
+        assertFalse((new App()).ifYes3("foo"));
+        assertTrue((new App()).ifYes3("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes4(String)}
+     */
+    @Test
+    void testIfYes4() {
+        assertFalse((new App()).ifYes4("foo"));
+        assertTrue((new App()).ifYes4("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes5(String)}
+     */
+    @Test
+    void testIfYes5() {
+        assertFalse((new App()).ifYes5("foo"));
+        assertTrue((new App()).ifYes5("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes6(String)}
+     */
+    @Test
+    void testIfYes6() {
+        assertFalse((new App()).ifYes6("foo"));
+        assertTrue((new App()).ifYes6("Yes"));
+    }
+
+    /**
+     * Method under test: {@link App#ifYes7(String)}
+     */
+    @Test
+    void testIfYes7() {
+        assertFalse((new App()).ifYes7("foo"));
+        assertTrue((new App()).ifYes7("Yes"));
+    }
 }
 
